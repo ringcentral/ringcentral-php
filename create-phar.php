ebcf25b5b2e4fbb509aa2fab199caba2b94d01b4
Kirill Konshin <kirill.konshin@gmail.com>
2015-03-03 17:53:10 -0800

    PHAR, autoload has been changed to PSR-4 from PSR-0

---
+++ create-phar.php
@@ -0,0 +1,24 @@
+<?php
+
+$phar = new Phar("./dist/rcsdk.phar",
+    FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
+    "rcsdk.phar");
+
+$phar["autoload.php"] = file_get_contents("./lib/autoload.php");
+$phar["RC/SDK.php"] = file_get_contents("./lib/RC/SDK.php");
+$phar["RC/platform/Auth.php"] = file_get_contents("./lib/RC/platform/Auth.php");
+$phar["RC/platform/Parser.php"] = file_get_contents("./lib/RC/platform/Parser.php");
+$phar["RC/platform/Platform.php"] = file_get_contents("./lib/RC/platform/Platform.php");
+
+$phar->setStub($phar->createDefaultStub("autoload.php"));
+
+//////////
+
+require('./_cache/guzzle.phar');
+require('./dist/rcsdk.phar');
+
+$sdk = new RC\SDK('foo', 'bar', 'http://server');
+
+$url = $sdk->getPlatform()->apiUrl('/foo', ['addServer' => true]);
+
+print $url . ': ' . ($url == 'http://server/restapi/v1.0/foo' ? 'true' : 'false') . PHP_EOL;
