2f6e85e706cbcf4af2236746a6d1c413e06a0c23
Kirill Konshin <kirill.konshin@gmail.com>
2015-04-17 15:06:25 -0700

    PHPSecLib, PHAR, Demos

---
+++ demo/subscription.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once(__DIR__ . '/_bootstrap.php');
+
+use RC\SDK;
+use RC\subscription\events\NotificationEvent;
+use RC\subscription\Subscription;
+
+$credentials = require(__DIR__ . '/_credentials.php');
+
+// Create SDK instance
+
+$rcsdk = new SDK($credentials['appKey'], $credentials['appSecret'], $credentials['server']);
+
+$platform = $rcsdk->getPlatform();
+
+// Authorize
+
+$platform->authorize($credentials['username'], $credentials['extension'], $credentials['password'], true);
+
+// Subscription
+
+$subscription = $rcsdk->getSubscription();
+
+$subscription->addEvents(array('/account/~/extension/~/message-store'));
+
+$subscription->setKeepPolling(false);
+
+$subscription->on(Subscription::EVENT_NOTIFICATION, function (NotificationEvent $e) {
+    print 'Notification' . print_r($e->getPayload(), true) . PHP_EOL;
+});
+
+print 'Subscribing' . PHP_EOL;
+
+$subscription->register();
+
+print 'End' . PHP_EOL;
+
