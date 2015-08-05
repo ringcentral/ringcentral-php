2743d301bb53325f6d53d886345afc918d6947f8
Kirill Konshin <kirill.konshin@gmail.com>
2015-08-05 02:09:53 -0700

    Replaced copied guzzle/guzzle parsers with guzzlehttp/psr7 functions

--- src/http/Transaction.php
+++ src/http/Transaction.php
@@ -3,7 +3,6 @@
 namespace RingCentral\http;
 
 use Exception;
-use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use stdClass;
@@ -44,24 +43,16 @@ class Transaction
     {
 
         $this->request = $request;
-        $this->body = $body;
+        $this->raw = $body;
 
-        if (is_string($body)) {
+        $body = (string)$body;
 
-            // Make the HTTP message complete
-            if (substr($body, 0, 5) !== 'HTTP/') {
-                $body = "HTTP/1.1 " . $status . "\r\n" . $body;
-            }
-
-            $r = $this->parseResponse($body);
-
-            if (!$r) {
-                throw new \InvalidArgumentException('Message was empty');
-            }
-
-            $this->response = new Response($r['code'], $r['headers'], $r['body'], $r['version'], $r['reason_phrase']);
-
+        // Make the HTTP message complete
+        if (substr($body, 0, 5) !== 'HTTP/') {
+            $body = "HTTP/1.1 " . $status . " OK\r\n" . $body;
         }
+
+        $this->response = \GuzzleHttp\Psr7\parse_response($body);
 
     }
 
@@ -161,7 +152,7 @@ class Transaction
 
             // Step 2. Split by boundary and remove first and last parts if needed
 
-            $parts = explode('--' . $boundary . '', (string)$this->response->getBody());
+            $parts = explode('--' . $boundary . '', (string)$this->response->getBody()); //TODO Handle as stream
 
             if (empty($parts[0])) {
                 array_shift($parts);
@@ -263,79 +254,6 @@ class Transaction
     protected function getContentType()
     {
         return $this->response->getHeaderLine('content-type');
-    }
-
-    /**
-     * Ported from guzzle/guzzle
-     * @param $message
-     * @return array
-     */
-    protected function parseMessage($message)
-    {
-        $startLine = null;
-        $headers = array();
-        $body = '';
-
-        // Iterate over each line in the message, accounting for line endings
-        $lines = preg_split('/(\\r?\\n)/', $message, -1, PREG_SPLIT_DELIM_CAPTURE);
-        for ($i = 0, $totalLines = count($lines); $i < $totalLines; $i += 2) {
-
-            $line = $lines[$i];
-
-            // If two line breaks were encountered, then this is the end of body
-            if (empty($line)) {
-                if ($i < $totalLines - 1) {
-                    $body = implode('', array_slice($lines, $i + 2));
-                }
-                break;
-            }
-
-            // Parse message headers
-            if (!$startLine) {
-                $startLine = explode(' ', $line, 3);
-            } elseif (strpos($line, ':')) {
-                $parts = explode(':', $line, 2);
-                $key = trim($parts[0]);
-                $value = isset($parts[1]) ? trim($parts[1]) : '';
-                if (!isset($headers[$key])) {
-                    $headers[$key] = $value;
-                } elseif (!is_array($headers[$key])) {
-                    $headers[$key] = array($headers[$key], $value);
-                } else {
-                    $headers[$key][] = $value;
-                }
-            }
-        }
-
-        return array(
-            'start_line' => $startLine,
-            'headers'    => $headers,
-            'body'       => $body
-        );
-    }
-
-    /**
-     * Ported from guzzle/guzzle
-     * @param $message
-     * @return array|bool
-     */
-    protected function parseResponse($message)
-    {
-        if (!$message) {
-            return false;
-        }
-
-        $parts = $this->parseMessage($message);
-        list($protocol, $version) = explode('/', trim($parts['start_line'][0]));
-
-        return array(
-            'protocol'      => $protocol,
-            'version'       => $version,
-            'code'          => $parts['start_line'][1],
-            'reason_phrase' => isset($parts['start_line'][2]) ? $parts['start_line'][2] : '',
-            'headers'       => $parts['headers'],
-            'body'          => $parts['body']
-        );
     }
 
 }
