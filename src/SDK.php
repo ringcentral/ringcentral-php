06f6f5af19a304ee7317843ada098631439afdbc
Kirill Konshin <kirill.konshin@gmail.com>
2017-01-04 15:46:04 -0800

    2.0.0

--- src/SDK.php
+++ src/SDK.php
@@ -12,7 +12,7 @@ use RingCentral\SDK\Subscription\Subscription;
 class SDK
 {
 
-    const VERSION = '2.0.0-rc1';
+    const VERSION = '2.0.0';
     const SERVER_PRODUCTION = 'https://platform.ringcentral.com';
     const SERVER_SANDBOX = 'https://platform.devtest.ringcentral.com';
 
