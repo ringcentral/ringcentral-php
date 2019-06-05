984c7836c05ec987dbc26abdc78fe4fd0aad65c5
Kirill Konshin <kirill.konshin@gmail.com>
2019-06-05 11:16:20 -0700

    Deps update

--- src/SDK.php
+++ src/SDK.php
@@ -12,7 +12,7 @@ use RingCentral\SDK\Subscription\Subscription;
 class SDK
 {
 
-    const VERSION = '2.1.2';
+    const VERSION = '2.2.2';
     const SERVER_PRODUCTION = 'https://platform.ringcentral.com';
     const SERVER_SANDBOX = 'https://platform.devtest.ringcentral.com';
 
